70dc0f9ea58e2521c796d39ae3d9e6b724b2be5d
Yury Byalik <yurybyalik@Yurys-MacBook-Pro-2.local>
2026-05-18 20:40:04 +0700

    Add Google Tag Manager (GTM-NPXJ7HLK) to (home5) layout

--- app/(home5)/layout.tsx
+++ app/(home5)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Unbounded } from 'next/font/google';
+import Script from 'next/script';
 import '../globals.css';
 import './home5.css';
 
@@ -43,7 +44,26 @@ export const metadata: Metadata = {
 export default function Home5Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${unbounded.variable} home5-root`}>{children}</body>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NPXJ7HLK');`}
+        </Script>
+      </head>
+      <body className={`${unbounded.variable} home5-root`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NPXJ7HLK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
